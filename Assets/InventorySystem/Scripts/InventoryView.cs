4ed683d91cc0a1a4e0fe442918d21ba0e4a6a4f5
stephanedurette <stephanedurette@rogers.com>
2026-02-27 17:17:22 -0500

    no message

--- Assets/InventorySystem/Scripts/InventoryView.cs
+++ Assets/InventorySystem/Scripts/InventoryView.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryView : MonoBehaviour
 {
-    
+    [Header("References")]
+    [SerializeField] private GridLayoutGroup gridLayoutGroup;
+
+    public Inventory BoundInventory { get; private set; }
+
+    public void Bind(Inventory inventory)
+    {
+        BoundInventory = inventory;
+    }
+
+    public void Unbind()
+    {
+        if (BoundInventory != null) { 
+            BoundInventory = null;
+        }
+    }
+
+    private void OnEnable()
+    {
+        //spawn slots and initialize them
+        //move scrollbar up
+        //populate slots with inventory
+    }
+
+    private void OnDisable()
+    {
+        Unbind();
+    }
 }
