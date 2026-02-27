e254af67cea9908623adcf3343046ec35ff6efe2
stephanedurette <stephanedurette@rogers.com>
2026-02-27 17:37:36 -0500

    no message

--- Assets/InventorySystem/Scripts/Inventory.cs
+++ Assets/InventorySystem/Scripts/Inventory.cs
@@ -1,4 +1,35 @@
+using System;
+
 public class Inventory
 {
+    public InventoryModel Model { get; private set; }
 
+    public Action<int, Item> OnItemRemoved;
+
+    public Action<int, Item> OnItemAdded;
+
+    private Item[] items;
+
+    public Inventory(InventoryModel model) {
+        this.Model = model;
+        items = new Item[Model.Size];
+    }
+
+    public void AddItem(Item item, int amount)
+    {
+
+    }
+
+    public void RemoveItem(Item item, int amount)
+    {
+
+    }
+
+    public void AddItem(Item item, int amount, int index) { 
+        //if new item bind the change function
+    }
+
+    public void RemoveItem(Item item, int amount, int index) { 
+        //unbind the item change function if amount < 0
+    }
 }
