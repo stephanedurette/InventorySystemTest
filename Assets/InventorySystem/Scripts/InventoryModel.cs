1e8e111af06dd9fecf14afbb5c7ef0001e195baa
stephanedurette <stephanedurette@rogers.com>
2026-02-27 17:05:01 -0500

    no message

---
+++ Assets/InventorySystem/Scripts/InventoryModel.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InventoryModel", menuName = "Scriptable Objects/InventoryModel")]
+public class InventoryModel : ScriptableObject
+{
+    public int Size;
+    public int ColumnCount;
+}
