a0885f36ef39c72db1b627210a202138ba6c78d1
stephanedurette <stephanedurette@rogers.com>
2026-02-28 11:10:59 -0500

    no message

--- Assets/InventorySystem/Scripts/InventoryView.cs
+++ Assets/InventorySystem/Scripts/InventoryView.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,26 +6,53 @@ public class InventoryView : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private GridLayoutGroup gridLayoutGroup;
+    [SerializeField] private Scrollbar scrollbar;
 
     public Inventory BoundInventory { get; private set; }
 
     public void Bind(Inventory inventory)
     {
         BoundInventory = inventory;
+        inventory.OnItemAdded += OnItemAdded;
+        inventory.OnItemRemoved += OnItemRemoved;
     }
 
     public void Unbind()
     {
-        if (BoundInventory != null) { 
+        if (BoundInventory != null) {
+            BoundInventory.OnItemAdded -= OnItemAdded;
+            BoundInventory.OnItemRemoved -= OnItemRemoved;
             BoundInventory = null;
         }
     }
 
+    private void OnItemAdded(int index, Item item)
+    {
+
+    }
+
+    private void OnItemRemoved(int index)
+    {
+
+    }
+
     private void OnEnable()
     {
+        InitializeWindow();
+    }
+
+    private void InitializeWindow()
+    {
         //spawn slots and initialize them
-        //move scrollbar up
+        StartCoroutine(SetScrollbarCoroutine());
         //populate slots with inventory
+    }
+
+    private IEnumerator SetScrollbarCoroutine()
+    {
+        //need to do this after UI calculations
+        yield return new WaitForEndOfFrame();
+        scrollbar.value = 1;
     }
 
     private void OnDisable()
