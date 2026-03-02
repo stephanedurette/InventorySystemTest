9468bfbb60c2c7fa024e3dfeb2131523e2244092
stephanedurette <stephanedurette@rogers.com>
2026-03-02 18:13:56 -0500

    no message

--- Assets/Game/Test Menu/Test Menu.cs
+++ Assets/Game/Test Menu/Test Menu.cs
@@ -1,16 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("References")]
+    [SerializeField] private Dropdown inventorySelectDropdown;
+    [SerializeField] private Dropdown itemSelectDropdown;
+    [SerializeField] private Dropdown indexSelectDropdown;
+    [SerializeField] private Slider itemAmountSlider;
+
+    [Header("Settings")]
+    [SerializeField] private List<ItemModel> itemSelectContent;
+    [SerializeField] private List<ItemModel> inventorySelectContent;
+
+    public void OnOpenInventoryClicked()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnAddItemClicked()
     {
-        
+
+    }
+
+    public void OnRemoveItemClicked()
+    {
+
     }
 }
