4bdc33ff9bc647530c38a7d1dc3dfbe0cb61c1d5
stephanedurette <stephanedurette@rogers.com>
2026-03-02 20:28:26 -0500

    no message

---
+++ Assets/Game/Scripts/InventoryHolder.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using Zenject;
+
+public class InventoryHolder : MonoBehaviour
+{
+    [SerializeField] private StartingItem[] startingItems;
+    [SerializeField] private InventoryModel inventoryModel;
+    [SerializeField] private Transform inventoryPosition;
+
+    [Serializable]
+    private struct StartingItem
+    {
+        public ItemModel Model;
+        public int Count;
+
+        public bool IsEmpty => Model == null || Count == 0;
+    }
+
+    private Inventory inventory;
+
+    private UIElementFactory uiFactory;
+
+    [Inject]
+    public void Construct(UIElementFactory uiFactory)
+    {
+        this.uiFactory = uiFactory;
+    }
+
+    private void Awake()
+    {
+        inventory = new(inventoryModel);
+
+        for (int i = 0; i < startingItems.Length; i++)
+        {
+            if (startingItems[i].IsEmpty) continue;
+            inventory.AddItem(startingItems[i].Model, startingItems[i].Count, i);
+        }
+    }
+
+    public void OpenInventory()
+    {
+        uiFactory.CreateInventoryWindow(inventoryPosition.position, inventory);
+    }
+}
--- Assets/Game/Test Menu/Test Menu.cs
+++ Assets/Game/Test Menu/Test Menu.cs
@@ -1,18 +1,19 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TestMenu : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private Dropdown inventorySelectDropdown;
-    [SerializeField] private Dropdown itemSelectDropdown;
-    [SerializeField] private Dropdown indexSelectDropdown;
+    [SerializeField] private TMP_Dropdown inventorySelectDropdown;
+    [SerializeField] private TMP_Dropdown itemSelectDropdown;
+    [SerializeField] private TMP_Dropdown indexSelectDropdown;
     [SerializeField] private Slider itemAmountSlider;
 
     [Header("Settings")]
     [SerializeField] private List<ItemModel> itemSelectContent;
-    [SerializeField] private List<ItemModel> inventorySelectContent;
+    [SerializeField] private List<InventoryHolder> inventorySelectContent;
 
     public void OnOpenInventoryClicked()
     {
