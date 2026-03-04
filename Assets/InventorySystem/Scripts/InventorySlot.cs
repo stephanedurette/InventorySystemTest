dba61425597b98164e7f605251849496fe60a22a
stephanedurette <stephanedurette@rogers.com>
2026-03-03 22:02:19 -0500

    no message

--- Assets/InventorySystem/Scripts/InventorySlot.cs
+++ Assets/InventorySystem/Scripts/InventorySlot.cs
@@ -7,6 +7,8 @@ public class InventorySlot : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
 
     private ItemView boundItemView;
 
+    public int Index => transform.GetSiblingIndex();
+
     public void Bind(ItemView boundItemView)
     {
         this.boundItemView = boundItemView;
@@ -16,11 +18,15 @@ public class InventorySlot : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("f");
+        //set dragged inventory item to bound item
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         Debug.Log("f");
+        //if dragged item and if this inventory is different than the dragged item's inventory
+            //remove the dragged item from that item's inventory
+            //add it to this slot's inventory at this slot's index
     }
 
     public void Unbind()
